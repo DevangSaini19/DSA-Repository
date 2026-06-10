22ebd8214bb5d16280ad24d24a1d664d626e75d6
DevangSaini19 <devangsaini46@gmail.com>
2026-06-10 19:12:19 +0530

    Add: SlidingWindow folder with corrected name

---
+++ SlidingWindow/MaxSumSubarrayofsizeK.java
@@ -0,0 +1,23 @@
+package SlidingWindow;
+
+
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        int l = 0;
+        int r = k-1;
+        int sum=0;
+       for(int i=0;i<=r;i++) {
+            sum = sum + arr[i];
+       }
+       int currentsum = sum;
+        int maxsum = sum;
+        while(r<arr.length-1) {
+            currentsum  = currentsum - arr[l] + arr[r + 1];
+            l++;
+            r++;
+            maxsum = Math.max(maxsum, currentsum);
+        }
+        return maxsum;
+        
+    }
+}
