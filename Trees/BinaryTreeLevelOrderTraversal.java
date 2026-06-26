6189ee5fd4404fec70d55c494f90cedc38605e18
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-06-26 15:57:49 +0530

    Solved BinaryTreeLevelOrderTraversal.java

---
+++ Trees/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i=0;i<size;i++) {
+                if (root == null) {
+                return new ArrayList<>();
+                }
+                TreeNode curr = q.poll();
+                if(curr.left!=null) {
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null) {
+                    q.offer(curr.right);
+                }
+                level.add(curr.val);
+            }
+            list.add(level);
+        }
+        return list;
+    }
+}
