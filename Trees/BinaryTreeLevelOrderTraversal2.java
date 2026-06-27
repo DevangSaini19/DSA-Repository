09affbc483f7724665cd588baa3f2bdc9cd18920
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-06-27 15:10:30 +0530

    Solved BinaryTreeLevelOrderTraversal2.java

--- Trees/BinaryTreeLevelOrderTraversal2.java
+++ Trees/BinaryTreeLevelOrderTraversal2.java
@@ -2,14 +2,15 @@ class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         List<List<Integer>> list = new ArrayList<>();
-        q.offer(root);
+        if (root == null) {
+            return new ArrayList<>();
+            }
+            q.offer(root);
         while(!q.isEmpty()) {
             int size = q.size();
             List<Integer> level = new ArrayList<>();
             for(int i=0;i<size;i++) {
-                if (root == null) {
-                return new ArrayList<>();
-                }
+                
                 TreeNode curr = q.poll();
                 if(curr.left!=null) {
                     q.offer(curr.left);
