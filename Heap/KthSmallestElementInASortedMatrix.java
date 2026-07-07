8f93d8a0ed1a5140e3df7b1c926d66258b55d973
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-07-07 12:44:50 +0530

    Solved Kth Smallest Element in a Sorted Matrix using Min Heap

---
+++ Heap/KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,53 @@
+// Brute Force
+// class Solution {
+//     public int kthSmallest(int[][] matrix, int k) {
+//         PriorityQueue<Integer> q = new PriorityQueue<>((a,b)-> b - a);
+//         for(int i=0;i<matrix.length;i++) {
+//             for(int j=0;j<matrix[0].length;j++) {
+//                 int a = matrix[i][j];
+//                 q.offer(a);
+//                 if(q.size()>k) {
+//                 q.poll();
+//             }
+//             }   
+//         }
+//         return q.peek();  
+//     }
+// }
+
+class Solution { // Optimized Solution Using Min Heap
+    class Pair{
+        int r;
+        int c;
+        int val;
+        public Pair(int r,int c,int val) {
+            this.r = r;
+            this.c = c;
+            this.val = val;
+        }
+    }
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Pair> q = new PriorityQueue<>((a,b)-> a.val - b.val);
+        for(int i=0;i<matrix.length;i++) {
+                int a = matrix[i][0];
+                Pair p = new Pair(i,0,a);
+                q.offer(p);
+        }
+        int count = 0;
+        while(!q.isEmpty()) {
+            Pair s = q.poll();
+            count++;
+            if(count==k) {
+                return s.val;
+            }
+            else {
+            if(s.c + 1 < matrix[0].length) {
+            int v = matrix[s.r][s.c+1];
+            Pair b = new Pair(s.r,s.c+1,v);
+            q.offer(b);
+                }
+            }
+        }
+        return -1;  
+    }
+}
