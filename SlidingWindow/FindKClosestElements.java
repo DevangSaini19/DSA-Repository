3f24a0c990c8169d202d6d3472217b857dc0a291
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-07-07 15:21:24 +0530

    Solved K Closest Elements Using Sliding Window

---
+++ SlidingWindow/FindKClosestElements.java
@@ -0,0 +1,28 @@
+package SlidingWindow;
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int l = 0;
+        int r = arr.length-1;
+        while(k<r-l+1) {
+            if(Math.abs(arr[l]-x) < Math.abs(arr[r]-x)) {
+                r--;
+            }
+            else if(Math.abs(arr[l]-x) > Math.abs(arr[r]-x)) {
+                l++;
+            }
+            else  {
+                if(arr[l]<arr[r]) {
+                    r--;
+                }
+                else {
+                    l++;
+                }
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int i=l;i<=r;i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+}
