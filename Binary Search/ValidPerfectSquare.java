f64e42452c3ac0d40e6beffc5f30d5cc96d57131
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-07-10 15:39:33 +0530

    Solved Valid Perfect Square

---
+++ Binary Search/ValidPerfectSquare.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long l = 0;
+        long h = num;
+        while(l<=h) {
+            long mid = l+(h-l)/2;
+            if(mid*mid==num) {
+                return true;
+            }
+            else if(mid*mid<num) {
+                l = mid+1;
+            }
+            else {
+                h = mid - 1;
+            }
+        }
+        return false;
+    }
+}
