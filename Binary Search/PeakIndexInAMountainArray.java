2e583256388aef81319dd7956dacf974aa2c2ec2
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-07-11 11:19:21 +0530

    Removed unused variable in peakIndexInMountainArray method

--- Binary Search/PeakIndexInAMountainArray.java
+++ Binary Search/PeakIndexInAMountainArray.java
@@ -2,7 +2,6 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int l = 0;
         int h = arr.length-1;
-        int ans = 0;
         while(l<=h) {
             int mid = l+(h-l)/2;
             if(arr[mid]<arr[mid+1]) {
