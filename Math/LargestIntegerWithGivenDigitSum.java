3e25880e585362becae98c3c3a4720a7371d6706
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-07-26 10:15:18 +0530

    Solved Largest Integer With Given Digit Sum

---
+++ Math/LargestIntegerWithGivenDigitSum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int largestInteger(int n, int s) {
+    int[] arr = new int[n];
+        if(s>9*n) {
+            return -1;
+        }
+        if(s==0) {
+            return 0;
+        }
+        String ans = "";
+        for(int i=0;i<n;i++) {
+        if(s>=9) {
+            arr[i] = 9;
+            s = Math.abs(9 - s);
+        }
+            else {
+                arr[i] = s;
+                s = Math.abs(s-s);
+            }
+        }
+        for(int i=0;i<arr.length;i++) {
+            ans+=arr[i];
+        }
+        return Integer.parseInt(ans);
+    }
+}
