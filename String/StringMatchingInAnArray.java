36de966d1b134b24c1064b68ac79d25547d9961c
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-09-05 10:57:09 +0530

    Solved String Matching in an Array

---
+++ String/StringMatchingInAnArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<words.length;i++) {
+            String a = words[i];
+            for(int j=0;j<words.length;j++) {
+                if(a.contains(words[j]) && i!=j && !list.contains(words[j])) {
+                    list.add(words[j]);
+                }
+            }
+        }
+        return list;
+    }
+}
