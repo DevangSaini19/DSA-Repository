a25ac23505bc9564c2bafd31fcb30b9b89785a0d
DevangSaini19 <199187979+DevangSaini19@users.noreply.github.com>
2026-09-09 19:49:28 +0530

    Solved Longest Uncommon Subsequence I

---
+++ String/ LongestUncommonSubsequenceI.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findLUSlength(String a, String b) {
+    if(a.length()!=b.length()) {
+        return Math.max(a.length(),b.length());
+    }
+    if(a.equals(b)) {
+        return -1;
+    }
+    return a.length();
+    }
+}
